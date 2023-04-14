694a9a8f5fd71488f09e56c68a59b9788fa1a4f9
renkaigis <renkaigis@gmai.com>
2023-04-14 15:54:54 +0800

    add curtin

--- js/myCustom.js
+++ js/myCustom.js
@@ -15,12 +15,15 @@ function getDate()
     // 页面底部学校信息
     var mySchool=document.getElementById("mySchool")
     var myCollege=document.getElementById("myCollege")
-    mySchool.innerHTML="Northwest A&F University"
-    myCollege.innerHTML="College of Natural Resources and Environment"
+    // mySchool.innerHTML="Northwest A&F University"
+    mySchool.innerHTML="Curtin University"
+    myCollege.innerHTML="School of Design and the Built Environment"
+    // myCollege.innerHTML="College of Natural Resources and Environment"
 
     // 页面底部的联系方式
     var myEmail=document.getElementById("myEmail")    
     var myQQ=document.getElementById("myQQ")
-    myEmail.innerHTML="<a href='"+"mailto:renkaigis@gmail.com'"+">renkaigis@gmail.com</a>"
+    myEmail.innerHTML="<a href='"+"mailto:kai.ren@curtin.edu.au'"+">kai.ren@curtin.edu.au</a>"
+    // myEmail.innerHTML="<a href='"+"mailto:renkaigis@gmail.com'"+">renkaigis@gmail.com</a>"
     myQQ.innerHTML="QQ: 541638321"
 }
